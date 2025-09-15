724bcdf9d554f3197a42ffda3e15617bd7007b2e
Choosla <cnckddn0146@gmail.com>
2025-09-15 14:27:33 +0900

    #1 feat/swagger: swagger 설정 2

---
+++ src/main/java/com/campick/server/api/car/dto/CarCreateRequestDTO.java
@@ -0,0 +1,12 @@
+package com.campick.server.api.car.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarCreateRequestDTO {
+    private String carName;
+}

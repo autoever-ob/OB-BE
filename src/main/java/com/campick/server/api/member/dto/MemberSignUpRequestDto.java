65593d2e831cf25ccb09dddbb779107f41f4268a
Choosla <cnckddn0146@gmail.com>
2025-09-16 10:18:07 +0900

    #6 feat/Entity: 깃 워크플로우 작성

---
+++ src/main/java/com/campick/server/api/member/dto/MemberSignUpRequestDto.java
@@ -0,0 +1,56 @@
+package com.campick.server.api.member.dto;
+
+import com.campick.server.api.member.entity.Member;
+import com.campick.server.api.member.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberSignUpRequestDto {
+    
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+    
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    // 추후 추가
+//    @Pattern(
+//            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+//            message = "비밀번호는 영문 대소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다."
+//    )
+    private String password;
+    
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하여야 합니다.")
+    private String nickname;
+    
+    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
+    @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$",
+            message = "올바른 휴대폰 번호 형식이 아닙니다. (예: 010-1234-5678)")
+    private String mobileNumber;
+
+
+    public Member toEntity(String encodedPassword){
+        return Member.builder()
+                .email(email)
+                .password(encodedPassword)
+                .nickname(nickname)
+                .mobileNumber(mobileNumber)
+                .role(Role.ROLE_USER)
+                .profileImage(null) // 추후 이미지 추가시에
+                .description(null)
+                .deletedAt(null)
+                .isDeleted(false)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/campick/server/api/member/entity/Role.java
@@ -0,0 +1,5 @@
+package com.campick.server.api.member.entity;
+
+public enum Role {
+    ROLE_USER, ROLE_DEALER
+}

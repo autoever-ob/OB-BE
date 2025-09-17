89ac148233c1b1100701e0d684e394e65497bbe3
Choosla <cnckddn0146@gmail.com>
2025-09-17 20:11:01 +0900

    #26 fix/Merge_Properties: 머지 충돌 해결

--- src/main/java/com/campick/server/api/product/controller/ProductController.java
+++ src/main/java/com/campick/server/api/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.campick.server.api.product.controller;
 
 import com.campick.server.api.product.dto.AllProductResponseDto;
 import com.campick.server.api.product.dto.ProductCreateRequestDto;
+import com.campick.server.api.product.dto.ProductCreateWithImageRequestDto;
 import com.campick.server.api.product.service.ProductService;
 import com.campick.server.common.response.ApiResponse;
 import com.campick.server.common.response.SuccessStatus;
@@ -27,12 +28,13 @@ public class ProductController {
 
     }
   
-    @PostMapping("createwithimages",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Long> createProduct(@RequestPart("dto") ProductCreateRequestDto dto,
+    @PostMapping(name="create-with-images",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Long> createProductWithImages(@RequestPart("dto") ProductCreateWithImageRequestDto dto,
                                               @RequestPart("images") List<MultipartFile> images,
                                               @RequestPart("mainImage") MultipartFile mainImage) throws IOException {
-        Long productId = productService.createProduct(dto, images, mainImage);
+        Long productId = productService.createProductWithImages(dto, images, mainImage);
         return ResponseEntity.ok(productId);
+    }
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<AllProductResponseDto>>> getProducts() {
--- src/main/java/com/campick/server/api/product/dto/ProductCreateWithImageRequestDto.java
+++ src/main/java/com/campick/server/api/product/dto/ProductCreateWithImageRequestDto.java
@@ -1,17 +1,13 @@
 package com.campick.server.api.product.dto;
 
-import com.campick.server.api.car.entity.Car;
-import com.campick.server.api.member.entity.Member;
-import com.campick.server.api.product.entity.Product;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class ProductCreateRequestDto {
+public class ProductCreateWithImageRequestDto {
     private String mileage;          // 주행거리
     private String vehicleType;      // 차종
     private String vehicleModel;     // 자동차 브랜드
@@ -20,10 +16,8 @@ public class ProductCreateRequestDto {
     private String plateHash;        // 차량번호
     private String title;            // 제목
     private String description;      // 설명
-    private String mainProductImageUrl; // 메인 이미지
 
-    private List<String> productImageUrl; // 차량 이미지 URL 목록
-    private List<OptionDTO> option;       // 옵션 리스트
+    private List<ProductCreateRequestDto.OptionDTO> option;       // 옵션 리스트
 
     @Getter
     @NoArgsConstructor

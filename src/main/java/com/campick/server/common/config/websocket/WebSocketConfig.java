faf94aac6f4a2855faf00722cebe1ace5821fa95
Admin <admin@Adminui-MacBookPro-7.local>
2025-09-19 18:25:28 +0900

    fix: websocket config /ws -> /ws/** 이걸 왜... 그리고 인터셉터에 디버깅 코드

--- src/main/java/com/campick/server/common/config/websocket/WebSocketConfig.java
+++ src/main/java/com/campick/server/common/config/websocket/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws")
+        registry.addHandler(webSocketHandler, "/ws/**")
                 .addInterceptors(new MemberHandshakeInterceptor())
                 .setAllowedOrigins("*"); // CORS 허용
     }

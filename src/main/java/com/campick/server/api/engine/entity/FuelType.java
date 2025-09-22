4221b5985538b1ac32c61c7c1cdd37f9e484b095
Admin <admin@Adminui-MacBookPro-7.local>
2025-09-22 21:07:26 +0900

    fix: fueltype, transmission 한국어로

--- src/main/java/com/campick/server/api/engine/entity/FuelType.java
+++ src/main/java/com/campick/server/api/engine/entity/FuelType.java
@@ -1,5 +1,16 @@
 package com.campick.server.api.engine.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum FuelType {
-    GASOLINE, DIESEL, ELECTRIC, HYBRID
+    GASOLINE("가솔린"),
+    DIESEL("디젤"),
+    ELECTRIC("전기"),
+    HYBRID("하이브리드");
+
+    private final String korean;
+    FuelType(String korean) {
+        this.korean = korean;
+    }
 }

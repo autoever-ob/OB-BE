c6706bdc32f92f4b34afd2869bc3df5e89402ad4
Choosla <cnckddn0146@gmail.com>
2025-09-23 17:30:12 +0900

    #160 fix(Car): 여러개의 카에 대한 처리

--- src/main/java/com/campick/server/api/car/repository/CarRepository.java
+++ src/main/java/com/campick/server/api/car/repository/CarRepository.java
@@ -5,8 +5,11 @@ import com.campick.server.api.model.entity.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
     Optional<Car> findByModel(Model model);
+
+    List<Car> findCarsByModel(Model model);
 }

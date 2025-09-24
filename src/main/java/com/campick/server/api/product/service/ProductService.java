4b586895fd290c259ad809bcd4b9b49efbe499bd
Choosla <cnckddn0146@gmail.com>
2025-09-24 22:18:00 +0900

    #160 fix(MemberProduct): 프로덕트 오류 해결

--- src/main/java/com/campick/server/api/product/service/ProductService.java
+++ src/main/java/com/campick/server/api/product/service/ProductService.java
@@ -45,6 +45,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -82,7 +83,8 @@ public class ProductService {
             throw new NotFoundException(ErrorStatus.CAR_NOT_FOUND.getMessage());
         }
 
-        Car car = cars.get(0);
+        Random random = new Random();
+        Car car = cars.get(random.nextInt(cars.size()));
 
 
         Member member = memberRepository.findById(memberId)
